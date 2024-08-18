829a26c47af4fe3cf06b967878fd685cc013faf3
Brian O'Neill <boneill@lanl.gov>
2024-08-18 12:27:07 -0500

    work

---
+++ components/omega/src/ocn/OcnFinal.cpp
@@ -0,0 +1,22 @@
+//===--  -------------*- C++ -*-===//
+//
+//
+//===----------------------------------------------------------------------===//
+
+#include "TimeMgr.h"
+
+namespace OMEGA {
+
+int OcnFinalize(
+   TimeInstant &CurrTime ///< [in] current sim time
+//   State &CurrState, ///< [in] current model state
+) {
+
+   I4 RetVal = 0;
+
+   return RetVal;
+}
+
+} // end namespace OMEGA
+
+//===----------------------------------------------------------------------===//
---
+++ components/omega/src/ocn/OcnRun.cpp
@@ -0,0 +1,27 @@
+//===--  -------------*- C++ -*-===//
+//
+//
+//===----------------------------------------------------------------------===//
+
+#include "TimeMgr.h"
+
+namespace OMEGA {
+
+int OcnRun(
+   TimeInstant &CurrTime, ///< [inout] current sim time
+   TimeInterval &RunInterval, ///< [in] interval to advance model
+   Alarm &EndAlarm ///< [out] alarm to end simulation
+//   State &CurrState ///< [inout] current model state
+) {
+
+   I4 RetVal = 0;
+
+
+
+   return RetVal;
+}
+
+} // end namespace OMEGA
+
+
+//===----------------------------------------------------------------------===//
